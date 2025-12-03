c626dc9e022211c94e171e1c09b82e8940a05d9d
Imad Syed <creeperone23@gmail.com>
2025-12-02 21:57:30 -0500

    Initialized base components for the entire app flow

---
+++ components/collisions/CollisionCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, Text } from "react-native-paper";
+
+export const CollisionCard = () => {
+  return (
+    <Card>
+      <Card.Content>
+        <Text variant="bodyMedium">Hello</Text>
+      </Card.Content>
+    </Card>
+  );
+};

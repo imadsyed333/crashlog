77e74d6fe41d6ccb40ab865159d4ab270644e5e9
Imad Syed <creeperone23@gmail.com>
2025-12-07 21:44:58 -0500

    Implemented handy methods to quickly updated the list type parameters of the collision state object

--- store/collisionFormStore.ts
+++ store/collisionFormStore.ts
@@ -1,15 +1,23 @@
-import { Collision } from "@/types";
-import { nanoid } from "nanoid";
+import { Collision, Person, Vehicle } from "@/types";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
 
 interface CollisionFormStore extends Collision {
-  updateField: <K extends keyof Collision>(key: K, value: Collision[K]) => void;
+  updateCollisionField: <K extends keyof Collision>(
+    key: K,
+    value: Collision[K]
+  ) => void;
   setForm: (collision: Collision) => void;
+  addVehicle: (vehicle: Vehicle) => void;
+  updateVehicle: (id: string, vehicle: Vehicle) => void;
+  addWitness: (witness: Person) => void;
+  updateWitness: (id: string, witness: Person) => void;
   resetForm: () => void;
 }
 
 const defaultCollision: Collision = {
-  id: nanoid(),
+  id: uuidv4(),
   date: new Date(),
   location: "",
   description: "",
@@ -21,11 +29,27 @@ const defaultCollision: Collision = {
 
 export const useCollisionFormStore = create<CollisionFormStore>((set) => ({
   ...defaultCollision,
-  updateField: (key, value) => set({ [key]: value }),
+  updateCollisionField: (key, value) => set({ [key]: value }),
   setForm: (collision) =>
     set({
       ...collision,
     }),
+  addVehicle: (vehicle) =>
+    set((state) => ({
+      vehicles: [...state.vehicles, vehicle],
+    })),
+  updateVehicle: (id: string, vehicle: Vehicle) =>
+    set((state) => ({
+      vehicles: state.vehicles.map((v) => (v.id === id ? vehicle : v)),
+    })),
+  addWitness: (witness) =>
+    set((state) => ({
+      witnesses: [...state.witnesses, witness],
+    })),
+  updateWitness: (id: string, witness) =>
+    set((state) => ({
+      witnesses: state.witnesses.map((w) => (w.id === id ? witness : w)),
+    })),
   resetForm: () =>
     set({
       ...defaultCollision,

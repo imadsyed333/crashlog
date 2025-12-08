cc7365f224862956b6b120662401c79b53207301
Imad Syed <creeperone23@gmail.com>
2025-12-07 21:38:31 -0500

    Merged component themes to one file to avoid repetition while staying consistent

---
+++ themes.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  input: {
+    marginHorizontal: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  button: {
+    margin: 10,
+    borderRadius: 5,
+  },
+  datetimepicker: {
+    marginTop: 5,
+    marginHorizontal: 2,
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    borderRadius: 100,
+  },
+  errorbox: {
+    color: "red",
+    marginHorizontal: 10,
+  },
+});

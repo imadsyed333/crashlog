9c2aa7bb3c5fc3b68c7f19e405c60e3c9646a878
Imad Syed <creeperone23@gmail.com>
2025-12-07 21:39:22 -0500

    Added routes to vehicle list and form screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,31 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "My Collisions" }} />
-        <Stack.Screen
-          name="collisions/new/index"
-          options={{ title: "New Collision" }}
-        />
-      </Stack>
+      <PaperProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "My Collisions" }} />
+          <Stack.Screen
+            name="collisions/new/index"
+            options={{ title: "Description" }}
+          />
+          <Stack.Screen
+            name="collisions/new/vehicleListScreen"
+            options={{
+              title: "Vehicles",
+            }}
+          />
+          <Stack.Screen
+            name="collisions/new/vehicleFormScreen"
+            options={{
+              title: "New Vehicle",
+            }}
+          />
+        </Stack>
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }

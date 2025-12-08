4321aab80e2de7759ab5681fdde36a9c3f0cf937
Imad Syed <creeperone23@gmail.com>
2025-12-07 21:42:27 -0500

    Added function call to reset collision form before loading it

--- components/collisions/AddCollisionButton.tsx
+++ components/collisions/AddCollisionButton.tsx
@@ -1,26 +1,21 @@
+import { useCollisionFormStore } from "@/store/collisionFormStore";
+import { styles } from "@/themes";
 import { useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
 import { FAB } from "react-native-paper";
 
 export const AddCollisionButton = () => {
+  const { resetForm } = useCollisionFormStore();
   const router = useRouter();
   return (
     <FAB
       icon={"plus"}
       style={styles.fab}
-      onPress={() => router.navigate("/collisions/new")}
+      onPress={() => {
+        resetForm();
+        router.navigate("/collisions/new");
+      }}
       mode="flat"
     />
   );
 };
-
-const styles = StyleSheet.create({
-  fab: {
-    position: "absolute",
-    margin: 16,
-    right: 0,
-    bottom: 0,
-    borderRadius: 100,
-  },
-});

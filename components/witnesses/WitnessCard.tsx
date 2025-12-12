a320b1c77aec39bae8a51c8db8999939dc57c5f6
Imad Syed <creeperone23@gmail.com>
2025-12-11 23:58:48 -0500

    Implemented witness list card, displaying witness name and phone number

---
+++ components/witnesses/WitnessCard.tsx
@@ -0,0 +1,22 @@
+import { Person } from "@/types";
+import React from "react";
+import { Card, Text } from "react-native-paper";
+
+type WitnessCardProps = {
+  witness: Person;
+};
+const WitnessCard = ({ witness }: WitnessCardProps) => {
+  const { name, phoneNumber } = witness;
+  return (
+    <Card style={{ marginHorizontal: 10, marginTop: 10 }} mode="outlined">
+      <Card.Content>
+        <Text variant="titleMedium">{name}</Text>
+        <Text variant="bodyMedium" dataDetectorType={"phoneNumber"}>
+          {phoneNumber}
+        </Text>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default WitnessCard;

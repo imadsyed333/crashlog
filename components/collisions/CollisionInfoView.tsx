c3c88c89660d720ba459440ab0b6f27e3e9b49a0
imadsyed333 <creeperone23@gmail.com>
2026-01-07 01:22:50 -0500

    Experimental style changes in an attmept to enable scrollable accordions

--- components/collisions/CollisionInfoView.tsx
+++ components/collisions/CollisionInfoView.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { View } from 'react-native'
-import { List, Text } from 'react-native-paper'
-import VehicleList from '../vehicles/VehicleList'
-import WitnessList from '../witnesses/WitnessList'
+import React from "react";
+import { View } from "react-native";
+import { List, Text } from "react-native-paper";
+import VehicleList from "../vehicles/VehicleList";
+import WitnessList from "../witnesses/WitnessList";
 
 const CollisionInfoView = () => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <List.Accordion title="Details">
         <Text>Hello</Text>
       </List.Accordion>
       <List.Accordion title="Vehicle Information">
-        <VehicleList/>
+        <VehicleList />
       </List.Accordion>
       <List.Accordion title="Witness Information">
-        <WitnessList/>
+        <WitnessList />
       </List.Accordion>
     </View>
-  )
-}
+  );
+};
 
-export default CollisionInfoView
+export default CollisionInfoView;

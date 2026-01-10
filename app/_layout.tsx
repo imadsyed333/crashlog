d8c056073e18d57427fb44b4386a3bdd62da5a3c
imadsyed333 <creeperone23@gmail.com>
2026-01-10 16:47:35 -0500

    Updated title of review page to reflect multi-use of page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout() {
           <Stack.Screen
             name="collisions/form/submitScreen"
             options={{
-              title: "Add Collision",
+              title: "Review",
             }}
           />
         </Stack>

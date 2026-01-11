bb5457760ba2abd9fbc4fc8b0211e8e42e6869b4
imadsyed333 <creeperone23@gmail.com>
2026-01-10 22:34:21 -0500

    Added collision details to submit screen with its own card

--- components/collisions/CollisionInfoView.tsx
+++ components/collisions/CollisionInfoView.tsx
@@ -1,17 +1,44 @@
+import { styles } from "@/lib/themes";
 import { useCollisionFormStore } from "@/store/collisionFormStore";
 import React from "react";
 import { View } from "react-native";
-import { List, Text } from "react-native-paper";
+import { Card, List, Text } from "react-native-paper";
 import VehicleCard from "../vehicles/VehicleCard";
 import WitnessCard from "../witnesses/WitnessCard";
 
 const CollisionInfoView = () => {
   const { collision } = useCollisionFormStore();
-  const { vehicles, witnesses } = collision;
+  const { vehicles, witnesses, location, description, date } = collision;
+
+  const newDate = new Date(date);
   return (
     <View style={{ flex: 1 }}>
       <List.Accordion title="Details">
-        <Text>Hello</Text>
+        <Card
+          mode="outlined"
+          style={{
+            margin: 10,
+          }}
+        >
+          <Card.Content>
+            <Text variant="bodyLarge">
+              <Text style={styles.boldText}>Location: </Text>
+              {location}
+            </Text>
+            <Text variant="bodyLarge">
+              <Text style={styles.boldText}>Description: </Text>
+              {description}
+            </Text>
+            <Text variant="bodyLarge">
+              <Text style={styles.boldText}>Datetime: </Text>
+              {newDate.toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}{" "}
+              on {newDate.toDateString()}
+            </Text>
+          </Card.Content>
+        </Card>
       </List.Accordion>
       <List.Accordion title="Vehicles">
         {vehicles.map((vehicle) => (

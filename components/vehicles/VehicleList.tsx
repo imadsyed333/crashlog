8f30baa20d9cadc225ac35892cfbc32f222c1edd
imadsyed333 <creeperone23@gmail.com>
2026-01-18 01:09:54 -0500

    Replaced FlatList with SwipeListView with hidden delete button, preventing accidental presses and utilizing gestures

--- components/vehicles/VehicleList.tsx
+++ components/vehicles/VehicleList.tsx
@@ -1,24 +1,47 @@
+import { theme } from "@/lib/themes";
 import { useCollisionFormStore } from "@/store/collisionFormStore";
 import React from "react";
-import { FlatList } from "react-native";
-import { Text } from "react-native-paper";
+import { View } from "react-native";
+import { IconButton, Text } from "react-native-paper";
+import { SwipeListView } from "react-native-swipe-list-view";
 import VehicleCard from "./VehicleCard";
 
 const VehicleList = () => {
-  const { collision } = useCollisionFormStore();
+  const { collision, deleteVehicle } = useCollisionFormStore();
   const { vehicles } = collision;
   return (
     <>
       {vehicles.length > 0 && (
-        <FlatList
+        <SwipeListView
           data={vehicles}
           renderItem={({ item, index }) => (
             <VehicleCard vehicle={item} index={index} />
           )}
+          renderHiddenItem={({ item }) => (
+            <View
+              style={{
+                display: "flex",
+                height: "100%",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "flex-end",
+              }}
+            >
+              <IconButton
+                icon={"delete"}
+                onPress={() => deleteVehicle(item.id)}
+                mode="contained"
+                size={30}
+                iconColor={theme.colors.error}
+              />
+            </View>
+          )}
           style={{
             display: "flex",
             width: "100%",
+            padding: 10,
           }}
+          rightOpenValue={-75}
         />
       )}
       {vehicles.length === 0 && <Text variant="titleLarge">Add a vehicle</Text>}
--- components/witnesses/WitnessList.tsx
+++ components/witnesses/WitnessList.tsx
@@ -1,24 +1,47 @@
+import { theme } from "@/lib/themes";
 import { useCollisionFormStore } from "@/store/collisionFormStore";
 import React from "react";
-import { FlatList } from "react-native";
-import { Text } from "react-native-paper";
+import { View } from "react-native";
+import { IconButton, Text } from "react-native-paper";
+import { SwipeListView } from "react-native-swipe-list-view";
 import WitnessCard from "./WitnessCard";
 
 const WitnessList = () => {
-  const { collision } = useCollisionFormStore();
+  const { collision, deleteWitness } = useCollisionFormStore();
   const { witnesses } = collision;
   return (
     <>
       {witnesses.length > 0 && (
-        <FlatList
+        <SwipeListView
           data={witnesses}
           renderItem={({ item, index }) => (
             <WitnessCard witness={item} index={index} />
           )}
+          renderHiddenItem={({ item }) => (
+            <View
+              style={{
+                display: "flex",
+                height: "100%",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "flex-end",
+              }}
+            >
+              <IconButton
+                icon={"delete"}
+                onPress={() => deleteWitness(item.id)}
+                mode="contained"
+                size={30}
+                iconColor={theme.colors.error}
+              />
+            </View>
+          )}
           style={{
             display: "flex",
             width: "100%",
+            padding: 10,
           }}
+          rightOpenValue={-75}
         />
       )}
       {witnesses.length === 0 && (

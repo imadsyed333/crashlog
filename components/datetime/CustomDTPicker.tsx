1e000ccc73e78c54c5ca00529c12224456401393
imadsyed333 <creeperone23@gmail.com>
2026-01-24 16:10:39 -0500

    Merged return statements into one with conditional rendering

--- components/datetime/CustomDTPicker.tsx
+++ components/datetime/CustomDTPicker.tsx
@@ -17,87 +17,94 @@ const CustomDTPicker = () => {
     updateCollisionField("date", date || new Date());
   };
 
-  if (Platform.OS === "ios") {
-    return (
-      <RNDateTimePicker
-        value={new Date(date)}
-        onChange={(e, date) => setDate(e, date!)}
-        mode="datetime"
-        textColor="black"
-        style={styles.datetimepicker}
-      />
-    );
-  } else {
-    const showPicker = (mode: "date" | "time") => {
-      DateTimePickerAndroid.open({
-        value: new Date(date),
-        mode,
-        onChange: setDate,
-        is24Hour: false,
-      });
-    };
+  const showPicker = (mode: "date" | "time") => {
+    DateTimePickerAndroid.open({
+      value: new Date(date),
+      mode,
+      onChange: setDate,
+      is24Hour: false,
+    });
+  };
 
-    const showDatePicker = () => {
-      showPicker("date");
-    };
+  const showDatePicker = () => {
+    showPicker("date");
+  };
 
-    const showTimePicker = () => {
-      showPicker("time");
-    };
+  const showTimePicker = () => {
+    showPicker("time");
+  };
 
-    return (
-      <Card mode="outlined">
-        <Card.Content>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
+  return Platform.OS === "ios" ? (
+    <Card mode="outlined">
+      <Card.Content
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <RNDateTimePicker
+          value={new Date(date)}
+          onChange={(e, date) => setDate(e, date!)}
+          mode="datetime"
+          textColor="black"
+          style={styles.datetimepicker}
+          themeVariant="light"
+        />
+      </Card.Content>
+    </Card>
+  ) : (
+    <Card mode="outlined">
+      <Card.Content>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Text>
+            {newDate.toLocaleDateString([], {
+              weekday: "short",
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            })}
+          </Text>
+          <Text>
+            {newDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            onPress={showDatePicker}
+            mode="contained"
+            style={styles.button}
           >
-            <Text>
-              {newDate.toLocaleDateString([], {
-                weekday: "short",
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              })}
-            </Text>
-            <Text>
-              {newDate.toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </Text>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+            Pick Date
+          </Button>
+          <Button
+            onPress={showTimePicker}
+            mode="contained"
+            style={styles.button}
           >
-            <Button
-              onPress={showDatePicker}
-              mode="contained"
-              style={styles.button}
-            >
-              Pick Date
-            </Button>
-            <Button
-              onPress={showTimePicker}
-              mode="contained"
-              style={styles.button}
-            >
-              Pick Time
-            </Button>
-          </View>
-        </Card.Content>
-      </Card>
-    );
-  }
+            Pick Time
+          </Button>
+        </View>
+      </Card.Content>
+    </Card>
+  );
 };
 
 export default CustomDTPicker;

98d0687688eb192c6d9ee657f117ac309b08c5be
imadsyed333 <creeperone23@gmail.com>
2026-03-19 02:34:44 -0400

    Replaced SafeAreaProvider with SafeAreaView, introduced logic for dyanmic theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,27 @@
 import { theme } from "@/lib/themes";
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  const paperTheme =
+    colorScheme === "dark"
+      ? { ...MD3DarkTheme, colors: theme.dark }
+      : { ...MD3LightTheme, colors: theme.light };
   return (
-    <PaperProvider theme={theme}>
-      <SafeAreaProvider>
+    <PaperProvider theme={paperTheme}>
+      <SafeAreaView
+        style={{ flex: 1, backgroundColor: paperTheme.colors.background }}
+      >
         <Stack
           screenOptions={{
-            headerStyle: {
-              backgroundColor: theme.colors.primary,
-            },
-            headerTintColor: theme.colors.onPrimary,
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-            headerBackButtonDisplayMode: "minimal",
+            headerShown: false,
           }}
         />
-      </SafeAreaProvider>
+      </SafeAreaView>
     </PaperProvider>
   );
 }

5947f8eafcdc82e8115e7bc616d6a48aef6e971a
imadsyed333 <creeperone23@gmail.com>
2026-03-29 20:37:44 -0400

    Refactor RootLayout to use theme store for dynamic theming and added StatusBar management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,31 @@
 import { theme } from "@/lib/themes";
+import { useThemeStore } from "@/store/themeStore";
 import { Stack } from "expo-router";
-import { useColorScheme } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { theme: appTheme } = useThemeStore();
 
   const paperTheme =
-    colorScheme === "dark"
+    appTheme === "dark"
       ? { ...MD3DarkTheme, colors: theme.dark }
       : { ...MD3LightTheme, colors: theme.light };
   return (
-    <PaperProvider theme={paperTheme}>
-      <SafeAreaView
-        style={{ flex: 1, backgroundColor: paperTheme.colors.background }}
-      >
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
-      </SafeAreaView>
-    </PaperProvider>
+    <>
+      <StatusBar style={appTheme === "dark" ? "light" : "dark"} />
+      <PaperProvider theme={paperTheme}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: paperTheme.colors.background }}
+        >
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </SafeAreaView>
+      </PaperProvider>
+    </>
   );
 }

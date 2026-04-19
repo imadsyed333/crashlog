69bc979e9153ce940172aa0a4d495c20038a314d
imadsyed333 <creeperone23@gmail.com>
2026-04-18 22:14:38 -0400

    Added props to control size of image depending on container style

--- components/media/MediaCard.tsx
+++ components/media/MediaCard.tsx
@@ -2,21 +2,28 @@ import { styles } from "@/lib/themes";
 import { Media } from "@/lib/types";
 import { useCollisionFormStore } from "@/store/collisionFormStore";
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, ImageStyle, StyleProp, View, ViewStyle } from "react-native";
 import { IconButton, useTheme } from "react-native-paper";
 
 type MediaCardProps = {
   media: Media;
   showActions: boolean;
+  containerStyle?: StyleProp<ViewStyle>;
+  imageStyle?: StyleProp<ImageStyle>;
 };
 
-const MediaCard = ({ media, showActions }: MediaCardProps) => {
+const MediaCard = ({
+  media,
+  showActions,
+  containerStyle,
+  imageStyle,
+}: MediaCardProps) => {
   const { deleteMedia } = useCollisionFormStore();
 
   const theme = useTheme();
   return (
-    <View>
-      <Image source={{ uri: media.uri }} style={styles.image} />
+    <View style={containerStyle}>
+      <Image source={{ uri: media.uri }} style={[styles.image, imageStyle]} />
       {showActions && (
         <IconButton
           icon={"delete"}

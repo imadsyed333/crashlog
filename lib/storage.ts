a358f660dad79c400a2cd2ec4c73df9375447828
imadsyed333 <creeperone23@gmail.com>
2026-05-31 19:57:05 -0400

    Series of asynchronous functions for creating / fetching encryption key for intializing central mmkv storage instance

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,20 +1,59 @@
-import { createMMKV } from "react-native-mmkv";
+import * as Crypto from "expo-crypto";
+import * as SecureStore from "expo-secure-store";
+import { createMMKV, MMKV } from "react-native-mmkv";
 import { StateStorage } from "zustand/middleware";
 
-const storage = createMMKV({
-  id: "crashlog-storage",
-  encryptionType: "AES-256",
-});
+let secureStorageInstance: MMKV | null = null;
+const MMKV_KEY_ALIAS = "crashlog-encryption-key";
+
+const getOrCreateEncryptionKey = async (): Promise<string> => {
+  let key = await SecureStore.getItemAsync(MMKV_KEY_ALIAS);
+
+  if (!key) {
+    key =
+      Crypto.randomUUID().replace(/-/g, "") +
+      Crypto.randomUUID().replace(/-/g, "");
+
+    await SecureStore.setItemAsync(MMKV_KEY_ALIAS, key);
+  }
+  return key;
+};
+
+export const initializeSecureStorage = async (): Promise<MMKV> => {
+  if (secureStorageInstance) return secureStorageInstance;
+
+  try {
+    const encryptionKey = await getOrCreateEncryptionKey();
+
+    secureStorageInstance = createMMKV({
+      id: "crashlog-storage",
+      encryptionKey,
+    });
+    return secureStorageInstance;
+  } catch (e) {
+    console.error("Failed to securely initialize MMKV instance", e);
+    throw e;
+  }
+};
+
+export const getSecureStorage = async (): Promise<MMKV> => {
+  if (!secureStorageInstance) {
+    throw new Error(
+      "Storage has not been initialized. Call initializeSecureStorage first.",
+    );
+  }
+  return secureStorageInstance;
+};
 
 export const mmkvStorage: StateStorage = {
-  setItem: (name, value) => {
-    return storage.set(name, value);
+  setItem: async (name, value) => {
+    return (await getSecureStorage()).set(name, value);
   },
-  getItem: (name) => {
-    const value = storage.getString(name);
+  getItem: async (name) => {
+    const value = (await getSecureStorage()).getString(name);
     return value ?? null;
   },
-  removeItem: (name) => {
-    return storage.remove(name);
+  removeItem: async (name) => {
+    return (await getSecureStorage()).remove(name);
   },
 };

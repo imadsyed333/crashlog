21a03779ea0d28e6d09c66141fd1449b9ad57d97
imadsyed333 <creeperone23@gmail.com>
2026-05-31 16:49:47 -0400

    Updated testing setup to response to usage of unencrypted storage

--- __tests__/testUtils/resetStores.ts
+++ __tests__/testUtils/resetStores.ts
@@ -1,31 +1,16 @@
 import { useCollisionFormStore } from "@/store/collisionFormStore";
-import {
-  initializeCollisionStore,
-  useCollisionStore,
-} from "@/store/collisionStore";
+import { useCollisionStore } from "@/store/collisionStore";
 import { useThemeStore } from "@/store/themeStore";
 import { useVehicleFormStore } from "@/store/vehicleFormStore";
 import { useWitnessFormStore } from "@/store/witnessFormStore";
 
 export const resetAllStores = async () => {
-  // Reinitialize collision store
-  await initializeCollisionStore();
-
   // Clear mock MMKV storage
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const mmkvMock = require("react-native-mmkv");
   if (mmkvMock.storage) {
     Object.keys(mmkvMock.storage).forEach((id: string) => {
       mmkvMock.storage[id] = {};
-    });
-  }
-
-  // Clear mock expo-secure-store storage
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const secureStoreMock = require("expo-secure-store");
-  if (secureStoreMock.store) {
-    Object.keys(secureStoreMock.store).forEach((key: string) => {
-      delete secureStoreMock.store[key];
     });
   }
 

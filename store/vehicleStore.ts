5b683c513046ce24cb8ee005345859960a470067
imadsyed333 <creeperone23@gmail.com>
2026-06-18 17:10:02 -0400

    Added logic to check if app has asked user to prefill vehicle

--- store/vehicleStore.ts
+++ store/vehicleStore.ts
@@ -7,14 +7,20 @@ interface VehicleStore {
   vehicle: Vehicle | null;
   setVehicle: (v: Vehicle) => void;
   getVehicle: () => Vehicle | null;
+  askedForVehicle: boolean;
+  setAskedForVehicle: (value: boolean) => void;
+  getAskedForVehicle: () => boolean;
 }
 
 export const useVehicleStore = create<VehicleStore>()(
   persist(
     (set, get): VehicleStore => ({
       vehicle: null,
+      askedForVehicle: false,
       setVehicle: (v: Vehicle) => set({ vehicle: v }),
       getVehicle: () => get().vehicle,
+      setAskedForVehicle: (value: boolean) => set({ askedForVehicle: value }),
+      getAskedForVehicle: () => get().askedForVehicle,
     }),
     {
       name: "vehicle-storage",
